6c33a683950e2b9b2f5bdfb71f25a5e6e5d249d0
u25251920-commits <u25251920@tuks.co.za>
2025-08-16 19:43:53 +0200

    Add files via upload

---
+++ RainPredictor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INF164_Homework_Assingment_1_Group19
+{
+    public partial class RainPredictor : Form
+    {
+        public RainPredictor()
+        {
+            InitializeComponent();
+        }
+    }
+}
